f6cab9d64512418e8ae1235da1848da63f9a0223
Narae Kim <naraekimw@gmail.com>
2021-12-14 07:58:21 +0000

    Add recursion version of binary search

--- binarysearch/binary_search.py
+++ binarysearch/binary_search.py
@@ -13,7 +13,29 @@ def binary_search(sorted_list, data):
     return None
 
 
+def binary_search_recursion(sorted_list, data, index=0):
+    if len(sorted_list) == 1:
+        if sorted_list[0] == data:
+            return index
+        else:
+            return None
+    else:
+        mid = (len(sorted_list) - 1) // 2
+        guess = sorted_list[mid]
+        if guess == data:
+            return index + mid
+        elif guess < data:
+            return binary_search_recursion(sorted_list[mid + 1:], data, index + mid + 1)
+        else:
+            return binary_search_recursion(sorted_list[:mid], data, index)
+
+
 if __name__ == '__main__':
     test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     print(binary_search(test_list, 3))
+    print(binary_search(test_list, 9))
     print(binary_search(test_list, 11))
+
+    print(binary_search_recursion(test_list, 3))
+    print(binary_search_recursion(test_list, 9))
+    print(binary_search_recursion(test_list, 11))
